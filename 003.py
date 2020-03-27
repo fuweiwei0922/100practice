9bf7b19d267d8e710785dd1c2f823b5825708faf
fuweiwei0922 <1276562941@qq.com>
2020-03-27 12:39:11 +0800

    add 003 answer

--- 003.py
+++ 003.py
@@ -1,2 +1,30 @@
-实例003：完全平方数
-题目 一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+# 实例003：完全平方数
+# 题目 一个整数，它加上100后是一个完全平方数，再加上168又是一个完全平方数，请问该数是多少？
+
+import math
+'''
+#方法一: 速度慢  ：
+for i in range(-100,10000):
+    for j in range(1,10000):
+        for k in range(1,10000):
+            if (i+100)==j*j and (i+268)==k*k:
+                print(i)
+                
+                '''
+'''
+#方法二：
+import math
+for z in range(10000):
+    x=int(math.sqrt(100+z))
+    y=int(math.sqrt(268+z))
+    if (x*x==(100+z))and (y*y==(z+268)):
+        print(z)
+'''
+
+
+#方法三：
+for z in range(10000):
+    x=math.sqrt(100+z)
+    y=math.sqrt(268+z)
+    if x == int(x) and  y == int(y):
+        print(z)
